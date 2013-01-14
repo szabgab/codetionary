8a6b480d25a403fd92b802c231ef928de9002ff3
Gabor Szabo <gabor@szabgab.com>
2013-01-14 16:21:22 +0200

    partially implementing deleting a language

--- bin/codetionary.py
+++ bin/codetionary.py
@@ -6,15 +6,16 @@ dbpath = 'code.db'
 
 # perl perldoc   python pydoc
 # perl File::Basename::basename  python 
-# 1) add a term in a language  '--add perl perldoc'
-# 2) connect two terms in two languages '--add perl perldoc python pydoc'
-# 3) query a term in a language and a target language '--ask perl perldoc python'
-# 4) query a term in a language '--ask perl perldoc'  listing all the other languages where it has a translation
-# 5) list all the terms in a language '--list perl'  listing all the terms in perl
-# 6) list all the languages --list
-# 7) add a language '--add perl'
+# *) add a language '--add perl'
+# *) list all the languages '--list'
 
-# Avoid adding the same language twice in different case (e.g. alway check the lower case version) 
+# *) delete a language if it does not have any phrases '--delete perl'
+
+# *) add a term in a language  '--add perl perldoc'
+# *) connect two terms in two languages '--add perl perldoc python pydoc'
+# *) query a term in a language and a target language '--ask perl perldoc python'
+# *) query a term in a language '--ask perl perldoc'  listing all the other languages where it has a translation
+# *) list all the terms in a language '--list perl'  listing all the terms in perl
 
 # there should a version using a remote web-database
 # but there could be a local version of it as well,
@@ -42,15 +43,34 @@ def connect_to_database():
       a INTEGER, b INTEGER)''')
     conn.commit()  
   except sqlite3.OperationalError, e:
+    #print 'sqlite error: ', e.args[0]  # table companies already exists 
+    pass
+  pass
+
+def delete_language(name):
+  print 'Deleting ', name
+  try:
+    c.execute('''SELECT id, name FROM languages WHERE lower(name) = ?''', (name.lower(), ))
+    lang = c.fetchone()
+    if not lang:
+      print 'The language is not in the database'
+      return
+    c.execute('''SELECT COUNT(*) FROM phrases WHERE language = ?''', (lang[0], ))
+    counter = c.fetchone()
+    #print counter
+    print "Not yet implemented"
+  except sqlite3.OperationalError, e:
     print 'sqlite error: ', e.args[0]  # table companies already exists 
     pass
-
-  pass
+    
 
 def add_language(name):
   print 'Adding ', name
   #print name.lower()
   try:
+    # TODO:
+    # Avoid adding the same language twice in different case (e.g. alway check the lower case version) 
+    # could be done with an SQL constraint?
     c.execute('''SELECT name FROM languages WHERE lower(name) = ?''', (name.lower(), ))
     exists = c.fetchone()
     if exists:
@@ -71,12 +91,15 @@ def list_languages():
 def main():
   ap = argparse.ArgumentParser()
   ap.add_argument('--add', help='add a language')
+  ap.add_argument('--delete', help='delete a language')
   ap.add_argument('--list', help='list the available languages', action='store_true')
   args = ap.parse_args()
 
   connect_to_database()
   if args.add:
     add_language(args.add)
+  elif args.delete:
+    delete_language(args.delete)
   elif args.list:
     list_languages()
   else:
